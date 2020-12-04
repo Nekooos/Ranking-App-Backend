f4d4424fbf1e691c87ddc70118446d6f2cb4266d
UsagiDrops <kristofferkramer83@gmail.com>
2020-12-04 20:47:22 +0100

    fixes to get all controller test

--- src/test/java/se/ranking/controller/CompetitionControllerTest.java
+++ src/test/java/se/ranking/controller/CompetitionControllerTest.java
@@ -10,10 +10,12 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import se.ranking.model.Competition;
 import se.ranking.service.CompetitionServiceImpl;
 import se.ranking.util.TestUtil;
 
 import java.util.Collections;
+import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.head;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -21,7 +23,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class CompetitionControllerTest {
     @MockBean
     private CompetitionServiceImpl competitionService;
@@ -29,22 +30,23 @@ class CompetitionControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @BeforeAll
+    @BeforeEach
     public void Setup(){
         testUtil = new TestUtil();
     }
 
     @Test
-    @DisplayName("GET /competition/all")
+    @DisplayName("GET /competition/all Success")
     public void findAllCompetitionsTest() throws Exception {
+        List<Competition> competitions = testUtil.createXCompetitions(5, Collections.emptyList());
         Mockito.when(competitionService.findAll())
-                .thenReturn(testUtil.createXCompetitions(5, Collections.emptyList()));
+                .thenReturn(competitions);
 
         mockMvc.perform(get("/competition/all"))
                 .andExpect(status().is2xxSuccessful())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.size()").value(5))
-                .andExpect(jsonPath("$[0].id").value(1));
-
+                .andExpect(jsonPath("$[0].id").value(1))
+                .andExpect(jsonPath("$[4].id").value(5));
     }
 }
