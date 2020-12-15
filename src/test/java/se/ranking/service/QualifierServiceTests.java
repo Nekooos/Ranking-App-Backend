86ea3d9f88ee4585719cb004323fb451ae600e77
UsagiDrops <kristofferkramer83@gmail.com>
2020-12-15 19:26:42 +0100

    Patch Qualifier test

--- src/test/java/se/ranking/service/QualifierServiceTests.java
+++ src/test/java/se/ranking/service/QualifierServiceTests.java
@@ -1,6 +1,8 @@
 package se.ranking.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.fge.jsonpatch.JsonPatch;
 import com.github.fge.jsonpatch.JsonPatchException;
 import org.junit.jupiter.api.Assertions;
@@ -10,10 +12,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.internal.matchers.Any;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import se.ranking.model.Qualifier;
 import se.ranking.repository.QualifierRepository;
 import se.ranking.util.TestUtil;
+
+import java.io.IOException;
+import java.util.Optional;
 
 public class QualifierServiceTests {
     @InjectMocks
@@ -21,25 +27,29 @@ public class QualifierServiceTests {
     @Mock
     private QualifierRepository qualifierRepository;
     private TestUtil testUtil;
-    private MappingJackson2HttpMessageConverter converter;
 
     @BeforeEach
     public void setup() {
         qualifierService = new QualifierServiceImpl();
         MockitoAnnotations.initMocks(this);
         testUtil = new TestUtil();
-        converter = new MappingJackson2HttpMessageConverter();
     }
 
     @Test
-    public void patchQualifier() throws JsonPatchException, JsonProcessingException {
+    public void patchQualifier() throws JsonPatchException, IOException {
         Qualifier qualifier = testUtil.createQualifier();
-        Mockito.when(qualifierRepository.findById(1L)).thenAnswer(i -> i.getArguments()[0]);
-        String json = "{\"op\":\"replace\", \"path\":\"name\",\"value\":\"patchedName\"}";
-        JsonPatch jsonPatch = converter.getObjectMapper().convertValue(json, JsonPatch.class);
 
-        Qualifier patchedQualifier = qualifierService.patchQualifier(jsonPatch, 1L);
+        Mockito.when(qualifierRepository.findById(1L)).thenAnswer(arguments -> Optional.of(qualifier));
+        Mockito.when(qualifierRepository.save(Mockito.any())).thenAnswer(i -> i.getArguments()[0]);
 
-        Assertions.assertEquals("qualifier", patchedQualifier.getName());
+        String json = "[{\"op\": \"replace\", \"path\": \"/name\", \"value\": \"patchedName\"}]";
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(json);
+        JsonPatch jsonPatch = JsonPatch.fromJson(jsonNode);
+
+        Qualifier result = qualifierService.patchQualifier(jsonPatch, 1L);
+
+        Assertions.assertEquals("patchedName", result.getName());
     }
 }
